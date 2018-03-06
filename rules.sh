99d69cbe87e3f9597deeca7e745cd88579c11720
heraud_b <baptiste.heraud@epitech.eu>
2018-03-06 12:31:32 +0100

    Secure

--- rules.sh
+++ rules.sh
@@ -16,7 +16,7 @@ ${IPT} -P INPUT DROP
 # Les connexions destinées à être routées sont acceptées par défaut
 ${IPT} -P FORWARD ACCEPT
 # Les connexions sortantes sont acceptées par défaut
-${IPT} -P OUTPUT ACCEPT
+${IPT} -P OUTPUT DROP
 
 
 ######################
